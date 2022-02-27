0122fd84f91edcfc4079d2028e89b7358ede9811
Collo <mutaic84@gmail.com>
2022-02-27 17:39:18 +0300

    add a method to track if there are still other questions remaining

--- quiz_brain.py
+++ quiz_brain.py
@@ -7,8 +7,11 @@ class QuizBrain:
         self.quiz_list = question_list
         self.question_number = 0
 
+    def still_has_questions(self):
+        return self.question_number < len(self.quiz_list)
+
     def next_question(self):
         current_question = self.quiz_list[self.question_number]
         self.question_number += 1
-        input(f"Q.{self.question_number}: {current_question.text} (True or False): ")
+        input(f"Q.{self.question_number}: {current_question.text} (True or False)?: ")
 
