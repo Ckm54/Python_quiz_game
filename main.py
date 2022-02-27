7c85384bc6c19d0ffcd5befc5b91634a4bd3b9fa
Collo <mutaic84@gmail.com>
2022-02-27 17:02:13 +0300

    model question objects into a list

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+from question_model import Question
+from data import question_data
+
+question_database = []
+for question in question_data:
+    quiz_text = question["text"]
+    quiz_answer = question["answer"]
+    new_question = Question(quiz_text, quiz_answer)
+    question_database.append(new_question)
 
 
 
